b262ee05dc1e4a9265b82367d7c042088d1b2eb6
chanwoo7 <chanwoo9987@gmail.com>
2024-05-03 22:32:31 +0900

    Reinitialize Project

---
+++ src/main/java/com/healthshop/healthshop/HealthShopApplication.java
@@ -0,0 +1,13 @@
+package com.healthshop.healthshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthshop/healthshop/HealthShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthshop.healthshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
