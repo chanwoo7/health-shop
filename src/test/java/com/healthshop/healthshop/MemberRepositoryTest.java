37c033e0a433ebafede01dda5a2facf221da1697
chanwoo7 <chanwoo9987@gmail.com>
2024-05-06 14:34:00 +0900

    [test] MemberRepository 테스트 코드 작성

---
+++ src/test/java/com/healthshop/healthshop/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.healthshop.healthshop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member: " + (findMember == member));
+    }
+
+}
