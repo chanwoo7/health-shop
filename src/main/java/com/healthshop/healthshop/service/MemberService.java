53851e72cf83522a086e0c4a8ec06fb887a2713c
chanwoo7 <chanwoo9987@gmail.com>
2024-05-13 23:12:30 +0900

    [feat] 회원 서비스 기초 개발

---
+++ src/main/java/com/healthshop/healthshop/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.healthshop.healthshop.service;
+
+import com.healthshop.healthshop.domain.member.Member;
+import com.healthshop.healthshop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 단건 조회
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
