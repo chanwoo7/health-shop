36582c55c42439269a89e773d640f8ca2638fce5
chanwoo7 <chanwoo9987@gmail.com>
2024-05-14 14:41:33 +0900

    [refactor] 스프링 생성자 주입 방식으로 변경

--- src/main/java/com/healthshop/healthshop/repository/MemberRepository.java
+++ src/main/java/com/healthshop/healthshop/repository/MemberRepository.java
@@ -3,15 +3,16 @@ package com.healthshop.healthshop.repository;
 import com.healthshop.healthshop.domain.member.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
