c7629cc38f6905e2d889dd5a6f8627da675e70c1
chanwoo7 <chanwoo9987@gmail.com>
2024-05-16 11:47:23 +0900

    [feat] 주문배송지 생성 메서드 작성

--- src/main/java/com/healthshop/healthshop/domain/order/delivery/DeliveryAddress.java
+++ src/main/java/com/healthshop/healthshop/domain/order/delivery/DeliveryAddress.java
@@ -1,6 +1,7 @@
 package com.healthshop.healthshop.domain.order.delivery;
 
 import com.healthshop.healthshop.domain.Address;
+import com.healthshop.healthshop.domain.member.MemberAddress;
 import com.healthshop.healthshop.domain.order.Order;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -20,5 +21,23 @@ public class DeliveryAddress extends Address {
 
     @OneToOne(mappedBy = "deliveryAddress", fetch = FetchType.LAZY)
     private Order order;
+
+    //==생성 메서드==//
+    /**
+     * 회원 기본배송지로부터 복사
+     */
+    // TODO: 반복적인 코드 리팩토링 필요
+    public static DeliveryAddress createDeliveryAddress(MemberAddress memberAddress) {
+        DeliveryAddress deliveryAddress = new DeliveryAddress();
+        deliveryAddress.setStatus(DeliveryStatus.BEFORE_SHIPPING);
+//        deliveryAddress.setOrder  // Order 객체 생성 후 연결할 것
+        deliveryAddress.setAddressMain(memberAddress.getAddressMain());
+        deliveryAddress.setAddressDetail(memberAddress.getAddressDetail());
+        deliveryAddress.setZipcode(memberAddress.getZipcode());
+        deliveryAddress.setName(memberAddress.getName());
+        deliveryAddress.setPhone(memberAddress.getPhone());
+        deliveryAddress.setRequest(memberAddress.getRequest());
+        return deliveryAddress;
+    }
 
 }
