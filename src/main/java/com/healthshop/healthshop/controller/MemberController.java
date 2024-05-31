5a40dc57825457573cddbc23aed6551a4ffd43d7
chanwoo7 <chanwoo9987@gmail.com>
2024-06-01 01:24:00 +0900

    [feat] 회원가입 페이지 작성

---
+++ src/main/java/com/healthshop/healthshop/controller/MemberController.java
@@ -0,0 +1,40 @@
+package com.healthshop.healthshop.controller;
+
+import com.healthshop.healthshop.controller.form.MemberForm;
+import com.healthshop.healthshop.domain.member.Member;
+import com.healthshop.healthshop.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/signup";
+    }
+
+//    public String create(@Valid MemberForm form, BindingResult result) {
+//        if (result.hasErrors()) {
+//            return "members/createMemberForm";
+//        }
+//        Member member = new Member();
+//
+//        member.setName(form.getName());
+//        member.setLoginId(form.getLoginId());
+//        member.setPassword(form.getPassword());
+//        member.setEmail(form.getEmail());
+//        member.setPhone(form.getPhone());
+//
+//        memberService.join(member);
+//    }
+
+}
