ca980759bd212cf05eefd881aabbef0a54fc9026
chanwoo7 <chanwoo9987@gmail.com>
2024-06-04 23:38:52 +0900

    [feat] 회원가입 시 무결성 검사 로직 추가, 회원가입 구현 완료

--- src/main/java/com/healthshop/healthshop/controller/MemberController.java
+++ src/main/java/com/healthshop/healthshop/controller/MemberController.java
@@ -1,6 +1,6 @@
 package com.healthshop.healthshop.controller;
 
-import com.healthshop.healthshop.controller.form.MemberForm;
+import com.healthshop.healthshop.controller.form.SignupForm;
 import com.healthshop.healthshop.domain.member.Member;
 import com.healthshop.healthshop.service.MemberService;
 import jakarta.validation.Valid;
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,12 +19,34 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/signup")
-    public String createForm(Model model) {
-        model.addAttribute("memberForm", new MemberForm());
+    public String showSignupForm(Model model) {
+        model.addAttribute("signupForm", new SignupForm());
         return "member/signup";
     }
 
-//    public String create(@Valid MemberForm form, BindingResult result) {
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute("signupForm") @Valid SignupForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "member/signup";
+        }
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setLoginId(form.getLoginId());
+        member.setPassword(form.getPassword());
+        member.setEmail(form.getEmail());
+
+        memberService.join(member);
+
+        return "redirect:/signup/success";
+    }
+
+    @GetMapping("/signup/success")
+    public String showSignupSuccessForm() {
+        return "member/signupSuccess";
+    }
+
+//    public String create(@Valid SignupForm form, BindingResult result) {
 //        if (result.hasErrors()) {
 //            return "members/createMemberForm";
 //        }
