a55a4af849121a62965400183b2e75e4194fd7ec
chanwoo7 <chanwoo9987@gmail.com>
2024-06-13 17:46:56 +0900

    [feat] 모델에 items 데이터 추가하여, shop 페이지에 상품 목록 전달하도록 수정

--- src/main/java/com/healthshop/healthshop/controller/ItemController.java
+++ src/main/java/com/healthshop/healthshop/controller/ItemController.java
@@ -1,13 +1,25 @@
 package com.healthshop.healthshop.controller;
 
+import com.healthshop.healthshop.domain.item.Item;
+import com.healthshop.healthshop.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
 
+    private final ItemService itemService;
+
+    // TODO: ItemDto 리스트로 반환하는 것 고려할 것
     @GetMapping("/shop")
-    public String shop() {
+    public String shop(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
         return "shop";
     }
 
