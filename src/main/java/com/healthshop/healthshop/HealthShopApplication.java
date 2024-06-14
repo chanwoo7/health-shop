50240d9230b2dbe7b025eb5c826a6734acdef0f8
chanwoo7 <chanwoo9987@gmail.com>
2024-06-14 14:52:16 +0900

    [test] 페이지네이션 테스트용 더미 데이터 생성

--- src/main/java/com/healthshop/healthshop/HealthShopApplication.java
+++ src/main/java/com/healthshop/healthshop/HealthShopApplication.java
@@ -1,5 +1,7 @@
 package com.healthshop.healthshop;
 
+import com.healthshop.healthshop.util.ExecuteSQLFile;
+import com.healthshop.healthshop.util.GenerateDummyData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,9 @@ public class HealthShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthShopApplication.class, args);
+
+		// 페이지네이션 테스트용 더미 데이터 생성
+		GenerateDummyData.main(null);
 	}
 
 }
