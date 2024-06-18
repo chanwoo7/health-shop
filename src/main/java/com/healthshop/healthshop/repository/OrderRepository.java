80762b85170b3b9dc5ec19ce4d05e775d7fda032
chanwoo7 <chanwoo9987@gmail.com>
2024-06-18 16:30:30 +0900

    [chore] 불필요한 import문 제거

--- src/main/java/com/healthshop/healthshop/repository/OrderRepository.java
+++ src/main/java/com/healthshop/healthshop/repository/OrderRepository.java
@@ -1,12 +1,8 @@
 package com.healthshop.healthshop.repository;
 
 import com.healthshop.healthshop.domain.order.Order;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
