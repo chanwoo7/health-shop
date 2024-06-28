bbed442780c4766920ed01ab7f83ceaef2a799f5
chanwoo7 <chanwoo9987@gmail.com>
2024-06-28 14:41:09 +0900

    [feat] GET, POST할 때 ItemForm 사용하도록 추가

--- src/main/java/com/healthshop/healthshop/controller/ItemController.java
+++ src/main/java/com/healthshop/healthshop/controller/ItemController.java
@@ -1,22 +1,25 @@
 package com.healthshop.healthshop.controller;
 
 import com.healthshop.healthshop.controller.dto.ItemDto;
+import com.healthshop.healthshop.controller.form.ItemForm;
 import com.healthshop.healthshop.domain.item.Item;
 import com.healthshop.healthshop.service.ItemService;
 import com.healthshop.healthshop.util.ItemConverter;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ItemController {
 
     private final ItemService itemService;
@@ -53,8 +56,40 @@ public class ItemController {
     public String showEditItemForm(@PathVariable Long itemId, Model model) {
         Item item = itemService.findOne(itemId);
 
-        model.addAttribute("item", item);
+        ItemForm itemForm = new ItemForm();
+        itemForm.setItemId(itemId);  // 입력 불가
+        itemForm.setName(item.getName());
+        itemForm.setCategoryId(item.getCategoryId());
+        itemForm.setPrice(item.getPrice());
+        itemForm.setDiscountRate(item.getDiscountRate());
+        itemForm.setBrand(item.getBrand());
+        // itemForm.setImgPath(item.getImgPath());
+        itemForm.setDescription(item.getDescription());
+        itemForm.setStockQuantity(item.getStockQuantity());  // 직접 입력은 불가, 수량 증가 버튼으로 늘리도록
+
+        model.addAttribute("itemForm", itemForm);
         return "item/manage";
+    }
+
+    @PostMapping("/shop/item/manage/{itemId}")
+    public String editItemForm(@ModelAttribute("itemForm") @Valid ItemForm form,
+                               @PathVariable Long itemId,
+                               BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "item/manage";
+        }
+        Item item = itemService.findOne(itemId);
+        item.setName(form.getName());
+        // itemService.setItemCategoryById(itemId, form.getCategoryId());
+        item.setPrice(form.getPrice());
+        item.setDiscountRate(form.getDiscountRate());
+        item.setBrand(form.getBrand());
+        // item.setImgPath(form.getImgPath());
+        item.setDescription(form.getDescription());
+        item.setStockQuantity(item.getStockQuantity());
+        itemService.saveItem(item);
+
+        return "redirect:/shop/item/{itemId}";
     }
 
 }
