314a870a93f771eaf7a30abed4e20cb018237077
chanwoo7 <chanwoo9987@gmail.com>
2024-07-08 23:45:05 +0900

    [refactor] confirm 관련 js 함수를 한 파일에 정리

--- src/main/resources/static/js/delete-item-handler.js
+++
@@ -1,3 +0,0 @@
-function confirmDelete() {
-    return confirm("정말로 이 상품을 삭제하시겠습니까?\n삭제 후에는 복구할 수 없습니다.");
-}
--- src/main/resources/static/js/validate-form.js
+++ src/main/resources/static/js/validate-form.js
@@ -1,4 +1,13 @@
-function validateForm() {
+function validateCreateItemForm() {
+    window.removeEventListener('beforeunload', handleBeforeUnload);
+    return confirm("정말로 이 내용대로 상품을 생성하시겠습니까?");
+}
+
+function validateUpdateItemForm() {
     window.removeEventListener('beforeunload', handleBeforeUnload);
     return confirm("정말로 변경사항을 반영하시겠습니까?");
+}
+
+function validateDeleteItemForm() {
+    return confirm("정말로 이 상품을 삭제하시겠습니까?\n삭제 후에는 복구할 수 없습니다.");
 }
